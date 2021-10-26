58ef66a97db2e2b938cd6c14c3383ee8c41e39bd
ben <bpedrick@gmail.com>
2021-10-25 20:14:10 -0500

    Third Commit

---
+++ jobs.js
@@ -0,0 +1,17 @@
+  var example = [" Woodworker", "Photographer", "Marketer", "Programmer?", "3D Modeler", "Data Analyst"];
+
+  textSequence(0);
+
+  function textSequence(i) {
+
+    if (example.length > i) {
+      setTimeout(function() {
+        document.getElementById("sequence").innerHTML = example[i];
+        textSequence(++i);
+      }, 2000);
+
+    } else if (example.length == i) {
+      textSequence(0);
+    }
+
+  }s
